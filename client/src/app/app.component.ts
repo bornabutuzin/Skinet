a7dc265ca2a176a667fd91d72507df76155022e2
Borna Butuzin <bbutuzin@vub.hr>
2022-09-16 15:40:01 +0200

    Angular version downgrade

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Skinet';
+
+  constructor(private http:HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('http://localhost:5075/api/products').subscribe((response: any) =>{
+      console.log(response);
+    }, error =>{
+      console.log(error);
+    });
+  }
 }
