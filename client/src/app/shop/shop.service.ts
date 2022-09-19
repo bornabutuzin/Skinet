ce96cbdeaf1774bcd73ca954c197e0b0d36ce5dd
Borna Butuzin <bbutuzin@vub.hr>
2022-09-19 17:59:58 +0200

    End of Section 10

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { IBrand } from '../shared/models/brand';
 import { IPagination } from '../shared/models/pagination';
+import { IProduct } from '../shared/models/product';
 import { IType } from '../shared/models/productType';
 import { ShopParams } from '../shared/models/shopParams';
 
@@ -36,6 +37,9 @@ getProducts(shopParams : ShopParams){
           return response.body;
         })
     );
+}
+getProduct(id: number){
+  return this.http.get<IProduct>(this.baseUrl + 'products/' + id);
 }
 getBrands(){
   return this.http.get<IBrand[]>(this.baseUrl + 'products/brands');
